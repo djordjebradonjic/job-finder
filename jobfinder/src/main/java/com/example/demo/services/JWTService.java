5d88d560abd79ef9d2027db07fb859c3e47e6cfb
djordjebradonjic <djordjebradonjic99@gmail.com>
2025-03-19 18:43:30 +0100

    Token validation and expiration added

--- jobfinder/src/main/java/com/example/demo/services/JWTService.java
+++ jobfinder/src/main/java/com/example/demo/services/JWTService.java
@@ -2,8 +2,10 @@ package com.example.demo.services;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -50,7 +52,7 @@ public class JWTService {
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis()*60*60*30))
                 .and()
-                .signWith(getKey())
+                .signWith(getSignInKey())
                 .compact();
 
     }
@@ -58,5 +60,18 @@ public class JWTService {
     private Key getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public boolean isTokenValidated(String token , UserDetails userDetails){
+        final String username= extractUsername(token);
+        return (username.equals(userDetails.getUsername())  && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
     }
 }
