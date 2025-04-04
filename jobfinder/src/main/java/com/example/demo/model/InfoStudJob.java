a906b1ec8acc6057b3ae77bb3acbc87487448b53
djordjebradonjic <djordjebradonjic99@gmail.com>
2025-04-04 17:34:37 +0200

    Finished Infostud scraper service

--- jobfinder/src/main/java/com/example/demo/model/InfoStudJob.java
+++ jobfinder/src/main/java/com/example/demo/model/InfoStudJob.java
@@ -1,4 +1,97 @@
 package com.example.demo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InfoStudJob {
+    private String title;
+    private String company;
+    private String location;
+    private String expirationDate;
+    private List<String> tags;
+    private String companyReviewLink;
+    private String numberOfReviews;
+
+    public InfoStudJob() {
+        this.tags = new ArrayList<>();
+    }
+
+    public InfoStudJob(String title, String company, String location, String expirationDate, List<String> tags, String companyReviewLink, String numberOfReviews) {
+        this.title = title;
+        this.company = company;
+        this.location = location;
+        this.expirationDate = expirationDate;
+        this.tags = tags;
+        this.companyReviewLink = companyReviewLink;
+        this.numberOfReviews = numberOfReviews;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getCompanyReviewLink() {
+        return companyReviewLink;
+    }
+
+    public void setCompanyReviewLink(String companyReviewLink) {
+        this.companyReviewLink = companyReviewLink;
+    }
+
+    public String getNumberOfReviews() {
+        return numberOfReviews;
+    }
+
+    public void setNumberOfReviews(String numberOfReviews) {
+        this.numberOfReviews = numberOfReviews;
+    }
+
+    @Override
+    public String toString() {
+        return "InfoStudJob{" +
+                "title='" + title + '\'' +
+                ", company='" + company + '\'' +
+                ", location='" + location + '\'' +
+                ", expirationDate='" + expirationDate + '\'' +
+                ", tags=" + tags +
+                ", companyReviewLink='" + companyReviewLink + '\'' +
+                ", numberOfReviews='" + numberOfReviews + '\'' +
+                '}';
+    }
 }
--- jobfinder/src/main/java/com/example/demo/services/InfoStudScraperService.java
+++ jobfinder/src/main/java/com/example/demo/services/InfoStudScraperService.java
@@ -18,18 +18,19 @@ import java.util.NoSuchElementException;
 public class InfoStudScraperService {
 
     private final String baseURL = "https://poslovi.infostud.com/oglasi-za-posao-developer-software-engineer/beograd?category%5B0%5D=5&dist=50";
+    List<InfoStudJob> jobs = new ArrayList<>();
 
     @Autowired
     ChromeDriver driver;
 
     public List<InfoStudJob> scrape() {
-        List<InfoStudJob> jobs = new ArrayList<>();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         int jobNumber = 2;
         System.out.println("Loading page: " + baseURL);
+        driver.get(baseURL);
+
         while (true) {
             try {
-                driver.get(baseURL);
 
                 System.out.println("Job number: " + jobNumber);
                 String cssSelector = "#oglas_" + jobNumber;
@@ -37,10 +38,11 @@ public class InfoStudScraperService {
 
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(cssSelector)));
                 WebElement job = driver.findElement(By.cssSelector(cssSelector));
-                System.out.println("Job with ID=" + jobNumber );
+                System.out.println("Job with ID=" + jobNumber);
+                extractData(job);
                 jobNumber++;
 
-            } catch (NoSuchElementException e) {
+            } catch (Exception e) {
                 System.out.println("No more jobs  to find");
                 break;
 
@@ -48,4 +50,45 @@ public class InfoStudScraperService {
         }
         return jobs;
     }
+
+    public void extractData(WebElement job) {
+        WebElement titleElement = job.findElement(By.cssSelector(".job__title_container h2"));
+        String title = titleElement.getText().trim();
+
+        WebElement companyElement = job.findElement(By.cssSelector(".job-company"));
+        String company = companyElement.getText().trim();
+
+        WebElement locationElement = job.findElement(By.cssSelector(".job-location span"));
+        String location = locationElement.getText().trim();
+
+        WebElement jobExpirationDateElement = job.findElement(By.cssSelector(".job-expiration"));
+        String expirationDate = jobExpirationDateElement.getText().trim();
+
+        WebElement tagElement = job.findElement(By.className("__job_tags"));
+        List<WebElement> tagElements = tagElement.findElements(By.className("__jobtag"));
+        List<String> tags= new ArrayList<>();
+        for (WebElement tag : tagElements) {
+            if(!tag.getText().trim().isEmpty()) {
+                tags.add(tag.getText().trim() + ", ");
+            }
+        }
+
+
+
+        String reviewText = "/";
+        String numberOfReviews = "/";
+        String companyReview = "";
+        try {
+            WebElement companyReviewElement = job.findElement(By.cssSelector(".job-card-company-review"));
+            companyReview = companyReviewElement.getAttribute("href");
+            reviewText = companyReviewElement.getText().trim();
+            numberOfReviews = reviewText.split(" ")[0];
+        } catch (RuntimeException e) {
+            System.out.println("No reviews for this company");
+
+        }
+        InfoStudJob infoStudJob = new InfoStudJob( title,  company,  location, expirationDate,  tags,  companyReview,  numberOfReviews);
+        jobs.add(infoStudJob);
+    }
+
 }
