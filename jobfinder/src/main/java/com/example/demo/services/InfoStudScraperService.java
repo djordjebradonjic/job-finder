54865db1e6931a51728e5c3f5afe65680d6db108
djordjebradonjic <djordjebradonjic99@gmail.com>
2025-04-08 00:25:10 +0200

    InfoStud scraper service updated

--- jobfinder/src/main/java/com/example/demo/services/InfoStudScraperService.java
+++ jobfinder/src/main/java/com/example/demo/services/InfoStudScraperService.java
@@ -1,6 +1,10 @@
 package com.example.demo.services;
 
+import com.example.demo.model.Company;
 import com.example.demo.model.InfoStudJob;
+import com.example.demo.model.Job;
+import com.example.demo.model.JobDetails;
+import com.example.demo.repository.JobRepository;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,15 +24,18 @@ public class InfoStudScraperService {
     private final String baseURL = "https://poslovi.infostud.com/oglasi-za-posao-developer-software-engineer/beograd?category%5B0%5D=5&dist=50";
     List<InfoStudJob> jobs = new ArrayList<>();
 
+
     @Autowired
     ChromeDriver driver;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     public List<InfoStudJob> scrape() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         int jobNumber = 2;
         System.out.println("Loading page: " + baseURL);
         driver.get(baseURL);
-
         while (true) {
             try {
 
@@ -39,38 +46,63 @@ public class InfoStudScraperService {
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(cssSelector)));
                 WebElement job = driver.findElement(By.cssSelector(cssSelector));
                 System.out.println("Job with ID=" + jobNumber);
+                System.out.println("Extracting job: " + jobNumber);
                 extractData(job);
+                System.out.println("Finished job: " + jobNumber);
                 jobNumber++;
 
             } catch (Exception e) {
+                e.printStackTrace();
                 System.out.println("No more jobs  to find");
                 break;
 
             }
+
         }
         return jobs;
     }
 
     public void extractData(WebElement job) {
-        WebElement titleElement = job.findElement(By.cssSelector(".job__title_container h2"));
-        String title = titleElement.getText().trim();
+       String title = "/";
+        try {
+            WebElement titleElement = job.findElement(By.cssSelector(".job__title_container h2"));
+            title = titleElement.getText().trim();
+        }catch (Exception e){
+            System.err.println("No title element");
+        }
 
-        WebElement companyElement = job.findElement(By.cssSelector(".job-company"));
-        String company = companyElement.getText().trim();
-
-        WebElement locationElement = job.findElement(By.cssSelector(".job-location span"));
-        String location = locationElement.getText().trim();
-
-        WebElement jobExpirationDateElement = job.findElement(By.cssSelector(".job-expiration"));
-        String expirationDate = jobExpirationDateElement.getText().trim();
-
-        WebElement tagElement = job.findElement(By.className("__job_tags"));
-        List<WebElement> tagElements = tagElement.findElements(By.className("__jobtag"));
-        List<String> tags= new ArrayList<>();
-        for (WebElement tag : tagElements) {
-            if(!tag.getText().trim().isEmpty()) {
-                tags.add(tag.getText().trim() + ", ");
+        String company = "/";
+        try {
+            WebElement companyElement = job.findElement(By.cssSelector(".job-company"));
+            company = companyElement.getText().trim();
+        }catch (Exception e){
+            System.err.println("No company element");
+        }
+        String location= "/";
+        try {
+            WebElement locationElement = job.findElement(By.cssSelector(".job-location span"));
+            location = locationElement.getText().trim();
+        }catch (Exception e ){
+            System.err.println("No location element");
+        }
+        String expirationDate = "/";
+        try {
+            WebElement jobExpirationDateElement = job.findElement(By.cssSelector(".job-expiration"));
+            expirationDate = jobExpirationDateElement.getText().trim();
+        }catch (Exception e ){
+            System.err.println("No expiration element");
+        }
+        List<String> tags = new ArrayList<>();
+        try {
+            WebElement tagElement = job.findElement(By.className("__job_tags"));
+            List<WebElement> tagElements = tagElement.findElements(By.className("__jobtag"));
+            for (WebElement tag : tagElements) {
+                if (!tag.getText().trim().isEmpty()) {
+                    tags.add(tag.getText().trim() + ", ");
+                }
             }
+        }catch (Exception e){
+            System.err.println("No tags element");
         }
 
 
@@ -83,12 +115,33 @@ public class InfoStudScraperService {
             companyReview = companyReviewElement.getAttribute("href");
             reviewText = companyReviewElement.getText().trim();
             numberOfReviews = reviewText.split(" ")[0];
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
+            numberOfReviews="0";
             System.out.println("No reviews for this company");
 
         }
         InfoStudJob infoStudJob = new InfoStudJob( title,  company,  location, expirationDate,  tags,  companyReview,  numberOfReviews);
+        saveJob(infoStudJob);
         jobs.add(infoStudJob);
     }
 
+
+    private void saveJob(InfoStudJob infoStudJob ){
+        Company  company = new Company.CompanyBuilder(infoStudJob.getCompany())
+                .numberOfReviews(Integer.parseInt(infoStudJob.getNumberOfReviews()))
+                .reviewLink(infoStudJob.getCompanyReviewLink())
+                .build();
+        JobDetails details = new JobDetails.JobDetailsBuilder()
+                .location(infoStudJob.getLocation())
+                .expirationDate(infoStudJob.getExpirationDate())
+                .build();
+        Job job = new Job.JobBuilder(infoStudJob.getTitle())
+                .details(details)
+                .company(company)
+                .tags(infoStudJob.getTags())
+                .source("InfoStud")
+                .build();
+        jobRepository.save(job);
+
+    }
 }
