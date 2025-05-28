b9ec0021229acc570acaf89dfbf72ec2854d3c79
djordjebradonjic <djordjebradonjic99@gmail.com>
2025-05-28 11:40:30 +0200

    Implemented scheduled job fetching every 2 hours

---
+++ jobfinder/src/main/java/com/example/demo/services/JobSchedulerService.java
@@ -0,0 +1,39 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class JobSchedulerService {
+
+        @Autowired
+        ScraperService scraperService;
+
+        @Autowired
+        InfoStudScraperService infoStudScraperService;
+
+        @Autowired
+        JobertyScraperService jobertyScraperService;
+
+        @Autowired
+        JoobleApiService joobleApiService;
+
+        public JobSchedulerService() {
+
+        }
+
+        @Scheduled(fixedRate = 2 * 60 * 60 * 1000)
+        public void fetchJobsFromAllSources() {
+            System.out.println("Fetching jobs... " + LocalDateTime.now());
+           scraperService.scrape();
+           infoStudScraperService.scrape();
+           jobertyScraperService.scrape();
+            String keywords = "java";
+            String location = "Serbia";
+            joobleApiService.searchJobs(keywords, location);
+        }
+    }
+
